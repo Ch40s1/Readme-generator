705d3040f3640d5714380518140819825c9f18e7
ch40s1 <zaratedaniel67@gmail.com>
2023-07-25 14:52:22 -0500

    downloaded inquirer

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+// first I need to access inquirer to ask the questions
+// next I need to get access to the file system to get a hold of the readme
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
