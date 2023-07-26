630016b0ae5b0949506c80bb2523807897962b20
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-07-26 12:52:49 -0500

    node now displays readme content

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,14 @@
 // TODO: Include packages needed for this application
 // first I need to access inquirer to ask the questions
+// const inquirer = require('inquirer');
+// console.log(inquirer);
 // next I need to get access to the file system to get a hold of the readme
+const fs = require('fs')
+console.log(fs); 
+fs.readFile('../README.md', 'utf-8', (err,data) =>{
+  if (err) throw err;
+  console.log(data);
+})
 // TODO: Create an array of questions for user input
 const questions = [];
 
